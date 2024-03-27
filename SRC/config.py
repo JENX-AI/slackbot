dbfb44da79465322881e3232db4168478f7b9c6f
essteer <essteer@pm.me>
2024-03-27 17:31:47 +0800

    refactor: extract constants to config file, add docstrings, clean code

---
+++ SRC/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+MODEL = "togethercomputer/llama-2-13b-chat"
+MAX_TOKENS = 1000
+TEMPERATURE = 0.0
+TOP_K = 50
+TOP_P = 0.7
+REPETITION_PENALTY = 1.1
+
+SYSTEM_PROMPT = """Just be a general purpose LLM. Use professional corporate language."""
