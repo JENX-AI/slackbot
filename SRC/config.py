a43fefc5bdf495d7b2ca09112b6ae181995e0f7e
essteer <essteer@pm.me>
2024-04-02 14:53:20 +0800

    fix: Update prompt to prevent AI saying thanks for each question

--- SRC/config.py
+++ SRC/config.py
@@ -4,7 +4,7 @@ MODEL = "meta-llama/Llama-2-7b-chat-hf"
 # MODEL = "meta-llama/Llama-2-70b-chat-hf"
 MAX_TOKENS = 1024
 TEMPERATURE = 0.1
-TOP_K = 1
+TOP_K = 5
 TOP_P = 0.7
 REPETITION_PENALTY = 1.1
 
@@ -12,6 +12,7 @@ SYSTEM_PROMPT = """
 <s>[INST]<<SYS>>
 You are an AI assistant having a conversation with a human.
 Use concise and professional language to respond.
+Respond directly, do not thank the human for their question when you reply.
 If you do not know the answer to a question, state truthfully that you do not know.
 
 {history}<</SYS>>
@@ -19,5 +20,5 @@ If you do not know the answer to a question, state truthfully that you do not kn
 """
 
 CHATBOTS = {
-    "U06R7L77USZ": "csbm-bot"
+    "U06S474L338": "csbm-bot"
 }
