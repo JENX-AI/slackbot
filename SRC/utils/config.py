19f8072eb587051311dd5775e6c16abf2bd155b2
essteer <essteer@pm.me>
2024-04-04 13:16:52 +0800

    feat: Add model variants to config file

--- SRC/utils/config.py
+++ SRC/utils/config.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-MODEL = "meta-llama/Llama-2-7b-chat-hf"
-# MODEL = "meta-llama/Llama-2-13b-chat-hf"
-# MODEL = "meta-llama/Llama-2-70b-chat-hf"
+
+MODEL = "meta-llama/Llama-2-13b-chat-hf"
+# MODEL = "mistralai/Mistral-7B-Instruct-v0.1"
+# MODEL = "togethercomputer/alpaca-7b"
+# MODEL = "zero-one-ai/Yi-34B-Chat"
+# IMAGE_MODEL = "stabilityai/stable-diffusion-2-1"
+
 MAX_TOKENS = 1024
 TEMPERATURE = 0.1
 TOP_K = 5
@@ -19,6 +23,7 @@ If you do not know the answer to a question, state truthfully that you do not kn
 {input}[/INST]
 """
 
+# Not in use - for reference only
 CHATBOTS = {
     "U06S474L338": "csbm-bot"
 }
